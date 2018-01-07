8c254b551e30cc644b32bac9e26396adb973561a
Alexey Kuznetsov <axet@me.com>
2018-01-07 17:32:10 +0300

    initial commit

---
+++ app/src/main/java/com/github/axet/bookreader/MainActivity.java
@@ -0,0 +1,199 @@
+package com.github.axet.bookreader;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.geometerplus.android.fbreader.libraryService.BookCollectionShadow;
+import org.geometerplus.fbreader.Paths;
+import org.geometerplus.fbreader.book.Book;
+import org.geometerplus.fbreader.book.BookUtil;
+import org.geometerplus.fbreader.bookmodel.BookModel;
+import org.geometerplus.fbreader.fbreader.FBReaderApp;
+import org.geometerplus.fbreader.fbreader.FBView;
+import org.geometerplus.fbreader.formats.FormatPlugin;
+import org.geometerplus.fbreader.formats.PluginCollection;
+import org.geometerplus.zlibrary.core.application.ZLApplication;
+import org.geometerplus.zlibrary.core.application.ZLApplicationWindow;
+import org.geometerplus.zlibrary.core.util.SystemInfo;
+import org.geometerplus.zlibrary.core.view.ZLViewWidget;
+import org.geometerplus.zlibrary.text.hyphenation.ZLTextHyphenator;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    SystemInfo info;
+    FBReaderApp app;
+    Book book;
+    FBView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        info = Paths.systemInfo(this);
+
+        app = (FBReaderApp) FBReaderApp.Instance();
+        if (app == null) {
+            app = new FBReaderApp(info, new BookCollectionShadow());
+        }
+
+        app.setWindow(new ZLApplicationWindow() {
+            @Override
+            public void setWindowTitle(String title) {
+            }
+
+            @Override
+            public void showErrorMessage(String resourceKey) {
+            }
+
+            @Override
+            public void showErrorMessage(String resourceKey, String parameter) {
+            }
+
+            @Override
+            public ZLApplication.SynchronousExecutor createExecutor(String key) {
+                return null;
+            }
+
+            @Override
+            public void processException(Exception e) {
+            }
+
+            @Override
+            public void refresh() {
+            }
+
+            @Override
+            public ZLViewWidget getViewWidget() {
+                return (ZLViewWidget) findViewById(R.id.main_view);
+            }
+
+            @Override
+            public void close() {
+            }
+
+            @Override
+            public int getBatteryLevel() {
+                return 0;
+            }
+        });
+        app.initWindow();
+
+        view = (FBView) ZLApplication.Instance().getCurrentView();
+
+        book = loadFile("/sdcard/123.fb2");
+        loadBook(book);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    Book loadFile(String file) {
+        try {
+            final PluginCollection pluginCollection = PluginCollection.Instance(info);
+            Book book = new Book(-1, file, null, null, null);
+            BookUtil.reloadInfoFromFile(book, pluginCollection);
+            return book;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void loadBook(Book book) {
+        try {
+            final PluginCollection pluginCollection = PluginCollection.Instance(info);
+            FormatPlugin plugin = BookUtil.getPlugin(pluginCollection, book);
+            BookModel Model = BookModel.createModel(book, plugin);
+            ZLTextHyphenator.Instance().load(book.getLanguage());
+            view.setModel(Model.getTextModel());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
