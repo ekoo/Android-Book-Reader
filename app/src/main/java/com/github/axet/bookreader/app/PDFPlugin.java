9ca78d9b6f994c6630f031981254747d21222ba3
Alexey Kuznetsov <axet@me.com>
2018-01-15 19:27:21 +0300

    add pdfview

--- app/src/main/java/com/github/axet/bookreader/app/PDFPlugin.java
+++ app/src/main/java/com/github/axet/bookreader/app/PDFPlugin.java
@@ -1,10 +1,11 @@
 package com.github.axet.bookreader.app;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.Log;
 
 import com.tom_roush.pdfbox.cos.COSName;
@@ -15,8 +16,6 @@ import com.tom_roush.pdfbox.pdmodel.PDResources;
 import com.tom_roush.pdfbox.pdmodel.common.PDRectangle;
 import com.tom_roush.pdfbox.pdmodel.graphics.PDXObject;
 import com.tom_roush.pdfbox.pdmodel.graphics.image.PDImage;
-import com.tom_roush.pdfbox.pdmodel.graphics.image.PDImageXObject;
-import com.tom_roush.pdfbox.rendering.PDFRenderer;
 import com.tom_roush.pdfbox.rendering.PageDrawer;
 import com.tom_roush.pdfbox.text.PDFTextStripper;
 
@@ -30,6 +29,8 @@ import org.geometerplus.fbreader.formats.BuiltinFormatPlugin;
 import org.geometerplus.zlibrary.core.encodings.EncodingCollection;
 import org.geometerplus.zlibrary.core.filesystem.ZLFile;
 import org.geometerplus.zlibrary.core.image.ZLImage;
+import org.geometerplus.zlibrary.core.view.ZLView;
+import org.geometerplus.zlibrary.core.view.ZLViewEnums;
 import org.geometerplus.zlibrary.text.model.ExtensionEntry;
 import org.geometerplus.zlibrary.text.model.ZLImageEntry;
 import org.geometerplus.zlibrary.text.model.ZLTextMark;
@@ -38,6 +39,8 @@ import org.geometerplus.zlibrary.text.model.ZLTextParagraph;
 import org.geometerplus.zlibrary.text.model.ZLTextStyleEntry;
 import org.geometerplus.zlibrary.text.model.ZLVideoEntry;
 import org.geometerplus.zlibrary.text.view.ZLTextControlElement;
+import org.geometerplus.zlibrary.text.view.ZLTextFixedPosition;
+import org.geometerplus.zlibrary.text.view.ZLTextPosition;
 import org.geometerplus.zlibrary.ui.android.image.ZLBitmapImage;
 
 import java.io.File;
@@ -188,7 +191,7 @@ public class PDFPlugin extends BuiltinFormatPlugin {
             ZLBitmapImage image = new ZLBitmapImage(null) {
                 @Override
                 public Bitmap getBitmap() {
-                    return createBitmap(); // reduce memory impact
+                    return createBitmap(model.doc.getPage(index)); // reduce memory impact
                 }
             };
             Map<String, ZLImage> imageMap = new TreeMap<>();
@@ -198,13 +201,11 @@ public class PDFPlugin extends BuiltinFormatPlugin {
             entries.add(e);
         }
 
-        Bitmap createBitmap() {
-            PDPage p = model.doc.getPage(this.index);
-
+        public static Bitmap createBitmap(PDPage p) {
             PDRectangle cropBox = p.getCropBox();
 
-            Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-            Bitmap bm = Bitmap.createBitmap((int) cropBox.getWidth(), (int) cropBox.getHeight(), conf);
+            Bitmap bm = Bitmap.createBitmap((int) cropBox.getWidth(), (int) cropBox.getHeight(), Bitmap.Config.ARGB_8888);
+
             Canvas canvas = new Canvas(bm);
 
             Paint paint = new Paint();
@@ -218,7 +219,7 @@ public class PDFPlugin extends BuiltinFormatPlugin {
             return bm;
         }
 
-        void renderPage(PDPage page, Paint paint, Canvas canvas, PDRectangle cropBox) throws IOException {
+        public static void renderPage(PDPage page, Paint paint, Canvas canvas, PDRectangle cropBox) throws IOException {
             int rotationAngle = page.getRotation();
 
             if (rotationAngle != 0) {
@@ -270,22 +271,182 @@ public class PDFPlugin extends BuiltinFormatPlugin {
         }
     }
 
-    public static class PDFTextModel implements ZLTextModel {
-        PDDocument doc;
-        ArrayList<ZLTextParagraph> pars = new ArrayList<>();
+    public static class RenderPage {
+        public PDDocument doc;
+        public int pageNumber;
+        public int pageOffset;
+        public PDPage page;
+        public PDRectangle pageBox;
 
-        public PDFTextModel(Book book) {
+        public RenderPage(RenderPage r) {
+            doc = r.doc;
+            pageNumber = r.pageNumber;
+            pageOffset = r.pageOffset;
+            page = r.page;
+            pageBox = r.pageBox;
+        }
+
+        public RenderPage(PDDocument d, int n, int o) {
+            doc = d;
+            pageNumber = n;
+            pageOffset = o;
+            load();
+        }
+
+        public boolean next(float h) {
+            pageOffset += h;
+            if (pageOffset >= pageBox.getHeight()) {
+                pageOffset = 0;
+                pageNumber++;
+                if (pageNumber >= doc.getNumberOfPages())
+                    return false;
+                load();
+                return true;
+            }
+            return true;
+        }
+
+        public boolean prev(float h) {
+            pageOffset -= h;
+            if (pageOffset < 0) {
+                pageNumber--;
+                if (pageNumber < 0)
+                    return false;
+                load();
+                pageOffset = (int) (pageBox.getHeight() - h);
+                return true;
+            }
+            return true;
+        }
+
+        void load() {
+            page = doc.getPage(pageNumber);
+            pageBox = page.getCropBox();
+        }
+
+        public PDRectangle cropBox(float h) {
+            return new PDRectangle(0, pageBox.getHeight() - pageOffset - h, pageBox.getWidth(), h);
+        }
+
+        public boolean equals(int n, int o) {
+            return pageNumber == n && pageOffset == o;
+        }
+    }
+
+    public static class PDFView {
+        public PDDocument doc;
+        public int pageNumber;
+        public int pageOffset;
+        public int pageHeight;
+        Paint paint = new Paint();
+        Bitmap wallpaper;
+        int wallpaperColor;
+
+        public PDFView(Book book) {
             ZLFile f = BookUtil.fileByBook(book);
             try {
                 doc = PDDocument.load(new File(f.getPath()));
-                for (int i = 0; i < doc.getNumberOfPages(); i++) {
-                    // pars.add(new RenderTextParagraph(this, i));
-                    // pars.add(new EndTextParagraph(ZLTextParagraph.Kind.END_OF_SECTION_PARAGRAPH));
-                    pars.add(new TextParagraph(this, i));
-                    pars.add(new EndTextParagraph(ZLTextParagraph.Kind.END_OF_TEXT_PARAGRAPH));
-                }
+                FBReaderApp app = ((FBReaderApp) FBReaderApp.Instance());
+                ZLFile wallpaper = app.BookTextView.getWallpaperFile();
+                if (wallpaper != null)
+                    this.wallpaper = BitmapFactory.decodeStream(wallpaper.getInputStream());
+                wallpaperColor = app.BookTextView.getBackgroundColor().intValue();
             } catch (IOException e) {
                 throw new RuntimeException(e);
+            }
+        }
+
+        public void close() {
+        }
+
+        public void gotoPosition(ZLTextPosition p) {
+            if (p == null) {
+                pageNumber = 0;
+                pageOffset = 0;
+            } else {
+                pageNumber = p.getParagraphIndex();
+                pageOffset = p.getElementIndex();
+            }
+        }
+
+        public RenderPage getPageNumber(ZLViewEnums.PageIndex index) {
+            RenderPage r = new RenderPage(doc, pageNumber, pageOffset);
+            switch (index) {
+                case current:
+                    break;
+                case next:
+                    RenderPage rr = new RenderPage(r);
+                    if (rr.next(pageHeight))
+                        return rr;
+                    break;
+                case previous:
+                    rr = new RenderPage(r);
+                    if (rr.prev(pageHeight))
+                        return rr;
+                    break;
+            }
+            return r;
+        }
+
+        public void onScrollingFinished(ZLViewEnums.PageIndex index) {
+            RenderPage r = getPageNumber(index);
+            pageNumber = r.pageNumber;
+            pageOffset = r.pageOffset;
+        }
+
+        public ZLTextFixedPosition getPosition() {
+            return new ZLTextFixedPosition(pageNumber, pageOffset, 0);
+        }
+
+        public boolean canScroll(ZLView.PageIndex index) {
+            return !getPageNumber(index).equals(pageNumber, pageOffset);
+        }
+
+        public void drawOnBitmap(Bitmap bitmap, int w, int h, ZLView.PageIndex index) {
+            Canvas canvas = new Canvas(bitmap);
+
+            if (wallpaper != null) {
+                float dx = wallpaper.getWidth();
+                float dy = wallpaper.getHeight();
+                for (int cw = 0; cw < bitmap.getWidth() + dx; cw += dx) {
+                    for (int ch = 0; ch < bitmap.getHeight() + dy; ch += dy) {
+                        canvas.drawBitmap(wallpaper, cw - dx, ch - dy, paint);
+                    }
+                }
+            } else {
+                canvas.drawColor(wallpaperColor);
+            }
+
+            RenderPage r = getPageNumber(index);
+
+            float rr = r.pageBox.getWidth() / w;
+            float hh = Math.min(h * rr, r.pageBox.getHeight());
+
+            pageHeight = (int) hh;
+            PDRectangle cropBox = r.cropBox(hh);
+            Bitmap bm = Bitmap.createBitmap((int) cropBox.getWidth(), (int) cropBox.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(bm);
+            try {
+                RenderTextParagraph.renderPage(r.page, paint, c, cropBox);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Rect src = new Rect(0, 0, bm.getWidth(), bm.getHeight());
+            Rect dst = new Rect(0, 0, w, h);
+            canvas.drawBitmap(bm, src, dst, paint);
+        }
+    }
+
+    public static class PDFTextModel extends PDFView implements ZLTextModel {
+        public ArrayList<ZLTextParagraph> pars = new ArrayList<>();
+
+        public PDFTextModel(Book book) {
+            super(book);
+            for (int i = 0; i < doc.getNumberOfPages(); i++) {
+                // pars.add(new RenderTextParagraph(this, i));
+                // pars.add(new EndTextParagraph(ZLTextParagraph.Kind.END_OF_SECTION_PARAGRAPH));
+                pars.add(new TextParagraph(this, i));
+                pars.add(new EndTextParagraph(ZLTextParagraph.Kind.END_OF_TEXT_PARAGRAPH));
             }
         }
 
