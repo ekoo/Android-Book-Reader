c519f54f2361b4eb54924a074a194d73481e866d
Alexey Kuznetsov <axet@me.com>
2018-06-17 17:15:36 +0300

    fix storage url

--- app/src/main/java/com/github/axet/bookreader/app/Storage.java
+++ app/src/main/java/com/github/axet/bookreader/app/Storage.java
@@ -1080,9 +1080,8 @@ public class Storage extends com.github.axet.androidlibrary.app.Storage {
                         }
                     }
                 }
-                Uri k = DocumentsContract.buildDocumentUriUsingTree(storage, book.md5 + "." + book.ext);
-                Uri root = Storage.getDocumentTreeUri(k);
-                Uri o = createFile(root, Storage.getDocumentChildPath(k));
+                String id = book.md5 + "." + book.ext;
+                Uri o = createFile(storage, id);
                 ContentResolver resolver = context.getContentResolver();
 
                 ParcelFileDescriptor fd = resolver.openFileDescriptor(o, "rw");
@@ -1094,7 +1093,7 @@ public class Storage extends com.github.axet.androidlibrary.app.Storage {
                 fis.close();
                 fos.close();
 
-                book.url = k;
+                book.url = o;
 
                 if (tmp)
                     file.delete();
